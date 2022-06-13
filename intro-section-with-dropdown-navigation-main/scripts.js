20f3eaf92496bfa36e8cd3034498d7d39ff396ff
Ronney Vargas <ronneyvargas@gmail.com>
2022-06-13 14:58:49 -0500

    Made some adjustments to addEventListener to show sideNav.

--- intro-section-with-dropdown-navigation-main/scripts.js
+++ intro-section-with-dropdown-navigation-main/scripts.js
@@ -6,13 +6,23 @@
 // document.getElementById("mySidenav").style.width = "0";
   
 
+let openNav = document.querySelector(".hamburger")
+let closeNav = document.getElementById("mySideNav")
+let mySideNav = document.getElementById("mySidenav")
 
 function myFunction() {
-  let openNav = document.getElementById("myLinks")
-  let closeNav = document.getElementById("mySideNav")
   if (openNav.style.display === "block") {
     openNav.style.display = "none";
   } else {
     openNav.style.display = "block"
   }
 }
+
+openNav.addEventListener('click', () => {
+  if (mySideNav.style.display === "block") {
+    mySideNav.style.display = "none";
+  } else {
+    mySideNav.style.display = "block"
+  }
+  console.log('Clicked hamburger');
+})
