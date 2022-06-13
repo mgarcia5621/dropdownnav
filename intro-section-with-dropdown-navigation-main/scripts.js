f5c618fec88c81a1d390454987afd0450dc483af
Mateo <matthew27garcia@gmail.com>
2022-06-13 13:44:42 -0500

    added some javascript

--- intro-section-with-dropdown-navigation-main/scripts.js
+++ intro-section-with-dropdown-navigation-main/scripts.js
@@ -1,7 +1,17 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+// function openNav() 
+// document.getElementById("mySidenav").style.width = "250px";
+  
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+// function closeNav() 
+// document.getElementById("mySidenav").style.width = "0";
+  
+
+
+function myFunction() {
+  let openNav = document.getElementById("myLinks")
+  if (openNav.style.display === "block") {
+    openNav.style.display = "none";
+  } else {
+    openNav.style.display = "block"
   }
+}
