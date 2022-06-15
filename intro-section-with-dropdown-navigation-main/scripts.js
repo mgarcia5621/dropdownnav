f1cf0c7703b07ee4263fdf5dac5a8d9ee4f2ca8b
Matthew Garcia <matthew27garcia@gmail.com>
2022-06-14 19:52:36 -0500

    added change to have button appear to close sidenac

--- intro-section-with-dropdown-navigation-main/scripts.js
+++ intro-section-with-dropdown-navigation-main/scripts.js
@@ -9,6 +9,8 @@
 let openNav = document.querySelector(".hamburger")
 let closeNav = document.getElementById("mySideNav")
 let mySideNav = document.getElementById("mySidenav")
+let openBtn = document.querySelector(".openbtn")
+let closeBtn = document.querySelector(".navbar2")
 
 function myFunction() {
   if (openNav.style.display === "block") {
@@ -21,8 +23,16 @@ function myFunction() {
 openNav.addEventListener('click', () => {
   if (mySideNav.style.display === "block") {
     mySideNav.style.display = "none";
+    closeBtn.style.display = "none";
+    openBtn.style.display = "block";
   } else {
-    mySideNav.style.display = "block"
+    mySideNav.style.display = "block";
+    openBtn.style.display = "none";
+    closeBtn.style.display = "block";
   }
   console.log('Clicked hamburger');
 })
+
+// function closeNav() {
+//   if (openNav)
+// }
